e25daad962cd61a157fd054c7262653a529cf7b6
SankaraMoothi <sankaramoorthi99@gmail.com>
2023-01-31 19:10:08 +0530

    addingLoader

--- src/Component/Questions/index.jsx
+++ src/Component/Questions/index.jsx
@@ -26,35 +26,41 @@ const Questions = () => {
   }
   return (
     <div className="QuestionContainer">
-      {question?.map((item) => {
-        return (
-          <div className="containerQuestion" key={item.id}>
-            <div className="viewVote">
-              <div className="vote">{item.like.length} votes</div>
-              <div className="answerCount">{item.answer_count}:answer</div>
-              <div className="viewCount">{item.views} view</div>
-            </div>
-            <div className="QuestCon">
-              <div
-                className="questionHeader"
-                onClick={() => handleQuestionDetails(item.id)}
-              >
-                {item.header}
-              </div>
-              <div className="questionTag">
-                {item.tags?.map((items) => {
-                  return <span key={items}>{items}</span>;
-                })}
-              </div>
+      {question.length <= 0 ? (
+        <h4 style={{ textAlign: "center", marginTop: "80px" }}>Loading....</h4>
+      ) : (
+        <>
+          {question?.map((item) => {
+            return (
+              <div className="containerQuestion" key={item.id}>
+                <div className="viewVote">
+                  <div className="vote">{item.like.length} votes</div>
+                  <div className="answerCount">{item.answer_count}:answer</div>
+                  <div className="viewCount">{item.views} view</div>
+                </div>
+                <div className="QuestCon">
+                  <div
+                    className="questionHeader"
+                    onClick={() => handleQuestionDetails(item.id)}
+                  >
+                    {item.header}
+                  </div>
+                  <div className="questionTag">
+                    {item.tags?.map((items) => {
+                      return <span key={items}>{items}</span>;
+                    })}
+                  </div>
 
-              <div className="userDetail">
-                <div className="Auth">{item.author}</div>
-                <div className="createdTime">{item.posted_on}</div>
+                  <div className="userDetail">
+                    <div className="Auth">{item.author}</div>
+                    <div className="createdTime">{item.posted_on}</div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        );
-      })}
+            );
+          })}
+        </>
+      )}
     </div>
   );
 };
